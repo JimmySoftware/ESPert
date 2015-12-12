d5178e47f8e913b11635c8e7b5da26efb41518c5
mozeal <jimmy@jimmysoftware.net>
2015-12-12 11:34:55 +0800

    ESPert workshop for Penang

---
+++ examples/_2000_ESPert_workshop/_2000_ESPert_workshop.ino
@@ -0,0 +1,167 @@
+#include <ESPert.h>
+
+ESPert espert;
+
+//IPAddress mqtt_server(192,168,77,1);
+const char* mqtt_server = "mqtt.espert.io";
+
+unsigned long loopTime = 0;
+
+int currentSwitch = true;
+String outTopic = "ESPert/" + String(espert.info.getChipId()) + "/Status";
+
+String inTopic = "ESPert/" + String(espert.info.getChipId()) + "/Command";;
+
+int currentSwitch2 = true;
+ESPert_Button button2;
+
+const char *host = "maker.ifttt.com";
+const int httpPort = 80;
+
+String ifttt_key;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  byte* p = (byte*)malloc(length+1);
+  // Copy the payload to the new buffer
+  memcpy(p,payload,length);
+  p[length] = 0;
+  String strPayload = String((char *)p);
+  free(p);
+  
+  espert.println("Receive: " + strPayload);
+  if (espert.json.init(strPayload) && espert.json.containsKey("LED")) {
+  
+    String value = espert.json.get("LED");
+
+    if (value == "0") {
+      espert.led.off();
+      espert.println("LED: Off");
+    } else if (value == "1") {
+      espert.led.on();
+      espert.println("LED: On");
+    } else if (value == "2") {
+      if( espert.led.isOn() ) {
+        espert.led.off();
+        espert.println("LED (Toglle): off");
+      }
+      else {
+        espert.led.on();
+        espert.println("LED (Toglle): On");
+      }
+    }
+
+    String outString  = "{\"LED\":\"" + String(espert.led.isOn() ? 1 : 0) + "\", ";
+    outString += "\"name\":\"" + String(espert.info.getId()) + "\"}";
+    espert.println("Send...: " + outString);
+    espert.mqtt.publish(outTopic, outString);
+  }
+  if (espert.json.init(strPayload) && espert.json.containsKey("Buzzer")) {
+  
+    String value = espert.json.get("Buzzer");
+    int l = value.toInt();
+    for( int i=0; i<l; i++ ) {
+      delay( 200 );
+      espert.buzzer.beep( 60, 200 );
+    }
+  }
+  if (espert.json.init(strPayload) && espert.json.containsKey("OLED")) {
+  
+    String value = espert.json.get("OLED");
+    espert.oled.clear();
+    espert.oled.println(value);
+
+  }
+  if (espert.json.init(strPayload) && espert.json.containsKey("IFTTT")) {
+  
+    String value = espert.json.get("IFTTT");
+    ifttt_key = value;
+    espert.eeprom.write( 200, `ifttt_key );
+  }
+}
+
+void setup() {
+  espert.init();
+  delay( 100 );
+  espert.buzzer.init( 15 );
+  espert.dht.init();
+  espert.mqtt.init(mqtt_server, 1883, callback);
+
+  ifttt_key = espert.eeprom.read( 200, 32 );
+
+  button2.init( 0 );
+
+  espert.oled.init();
+  delay(2000);
+
+  espert.println("Press USER button to turn on LED.");
+
+  espert.oled.clear();
+  espert.oled.println(espert.info.getId());
+  espert.oled.println();
+
+  int mode = espert.wifi.init();
+
+  if (mode == ESPERT_WIFI_MODE_CONNECT) {
+    espert.println(">>> WiFi mode: connected.");
+    espert.oled.println("WiFi: connected.");
+    espert.oled.print("IP..: ");
+    espert.oled.println(espert.wifi.getLocalIP());
+  } else if (mode == ESPERT_WIFI_MODE_DISCONNECT) {
+    espert.println(">>> WiFi mode: disconnected.");
+    espert.oled.println("WiFi: not connected.");
+  }
+
+  loopTime = millis();
+}
+
+void loop() {
+  espert.loop();
+  
+  if (espert.mqtt.connect()) {
+    espert.println("MQTT: Connected");
+    espert.println("MQTT: Out Topic " + outTopic);
+    espert.mqtt.subscribe(inTopic);
+    Serial.println("MQTT: Subscribed " + inTopic);
+  }
+
+  bool buttonPressed = espert.button.isOn();
+
+  if (buttonPressed != currentSwitch) {
+    String outString  = "{\"button\":\"" + String(buttonPressed ? 1 : 0) + "\", ";
+    outString += "\"name\":\"" + String(espert.info.getId()) + "\"}";
+    espert.println("Send...: " + outString);
+    espert.mqtt.publish(outTopic, outString);
+    currentSwitch = buttonPressed;
+  }
+
+  buttonPressed = button2.isOn();
+
+  if (buttonPressed != currentSwitch2) {
+    espert.println( "SW" );
+    if( buttonPressed ) {
+      String path = "/trigger/button/with/key/"+ifttt_key;
+      espert.led.on();
+      espert.println( ">>" + espert.wifi.getHTTP( host, path.c_str() ) + "<<" );
+      espert.led.off();
+    }
+    currentSwitch2 = buttonPressed;
+  }
+  
+  if( millis()-loopTime >= 5000 ) {
+    loopTime = millis();
+
+    float t = espert.dht.getTemperature();
+    float h = espert.dht.getHumidity();
+
+    if (!isnan(t) && !isnan(h)) {
+      String outString  = "{\"temperature\":\"" + String(t) + "\", ";
+      outString += "\"humidity\":\"" + String(h) + "\", ";
+      outString += "\"name\":\"" + String(espert.info.getId()) + "\"}";
+      espert.println(outString);
+      espert.mqtt.publish(outTopic, outString);
+    }
+
+  }
+  delay( 100 );
+}
+
