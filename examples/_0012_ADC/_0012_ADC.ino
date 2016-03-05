554df2ab68f32f1e952026f9669a32fa8d86db78
Nat Weerawan <nat.wrw@gmail.com>
2016-03-05 20:15:20 +0700

    use pre-defined LED_BUILTIN variable.

--- examples/_0012_ADC/_0012_ADC.ino
+++ examples/_0012_ADC/_0012_ADC.ino
@@ -1,14 +1,11 @@
-//#define ESPERT_BOARD_ESPRESSO_LITE2
-
-#ifdef ESPERT_BOARD_ESPRESSO_LITE2
-static const int PIN_LED = 2; // ESPresso Lite v2.0
-#else
-static const int PIN_LED = 16; // ESPresso Lite
-#endif
+/* 
+ * EPresso 1.0 LED_BUILTIN = 16;
+ * EPresso 2.0 LED_BUILTIN = 2;
+ */
 
 void setup() {
   Serial.begin(115200);
-  pinMode(PIN_LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(A0, INPUT);
   Serial.println("ADC init");
 }
